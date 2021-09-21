03d0692c27ebceda26ccf8d0953e318caadc988f
Carlos <carlaospa@gmail.com>
2021-09-20 22:52:51 -0300

    Projeto Angular Spring

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'crud-angular';
+}
